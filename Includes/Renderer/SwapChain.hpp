7aaf4a332e1c7eb3e9291d387a5cd33612c2e287
snowapril <sinjihng@naver.com>
2021-06-25 20:32:30 +0900

    feat : add swapchain class and init method

--- Includes/Renderer/SwapChain.hpp
+++ Includes/Renderer/SwapChain.hpp
@@ -1,14 +1,30 @@
 #ifndef SWAPCHAIN_HPP
 #define SWAPCHAIN_HPP
 
+#include <vulkan/vulkan.h>
 #include <Renderer/Resource.hpp>
+#include <memory>
+#include <vector>
 
 namespace Renderer
 {
+class Device;
 class SwapChain : public Resource
 {
  public:
+    //! Default constructor
+    SwapChain(std::shared_ptr<Device> devicePtr, VkExtent2D extent);
+    //! Default destructor
+    ~SwapChain();
+    //! Initialize swapchain
+    bool Initialize(std::shared_ptr<Device> devicePtr, VkExtent2D extent);
+
  protected:
+    VkSwapchainKHR _swapChain;
+    VkFormat _swapChainImageFormat;
+    std::vector<VkImage> _swapChainImages;
+    std::vector<VkImageView> _swapChainImageViews;
+
  private:
 };
 };  // namespace Renderer
---
+++ Sources/Renderer/SwapChain.cpp
@@ -0,0 +1,47 @@
+#include <vkbootstrap/VkBootstrap.h>
+#include <Renderer/Device.hpp>
+#include <Renderer/SwapChain.hpp>
+
+namespace Renderer
+{
+SwapChain::SwapChain(std::shared_ptr<Device> devicePtr, VkExtent2D extent)
+{
+    assert(Initialize(devicePtr, extent));
+}
+
+SwapChain::~SwapChain()
+{
+    //! Do nothing
+}
+
+bool SwapChain::Initialize(std::shared_ptr<Device> devicePtr, VkExtent2D extent)
+{
+    vkb::SwapchainBuilder builder{ devicePtr->GetPhysicalDevice(),
+                                   devicePtr->GetDevice(),
+                                   devicePtr->GetSurface() };
+
+    vkb::Swapchain vkbSwapChain =
+        builder.use_default_format_selection()
+            .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+            .set_desired_extent(extent.width, extent.height)
+            .build()
+            .value();
+
+    //! Store swapchain and its related images
+    _swapChain = vkbSwapChain.swapchain;
+    _swapChainImages.swap(vkbSwapChain.get_images().value());
+    _swapChainImageViews.swap(vkbSwapChain.get_image_views().value());
+    _swapChainImageFormat = vkbSwapChain.image_format;
+
+    PushDeletionCall([=]() {
+        vkDestroySwapchainKHR(devicePtr->GetDevice(), _swapChain, nullptr);
+        for (auto& image : _swapChainImages)
+            vkDestroyImage(devicePtr->GetDevice(), image, nullptr);
+        for (auto& imageView : _swapChainImageViews)
+            vkDestroyImageView(devicePtr->GetDevice(), imageView, nullptr);
+    });
+
+    return true;
+}
+
+};  // namespace Renderer
