58dfbe6570477bb14a12ee924c288843341323d5
snowapril <sinjihng@naver.com>
2021-06-25 19:51:50 +0900

    feat : manually delete copy, move assignment operator & constructor

--- Includes/Renderer/Resource.hpp
+++ Includes/Renderer/Resource.hpp
@@ -14,6 +14,11 @@ class Resource
     Resource();
     //! Default virtual destructr
     virtual ~Resource();
+    //! Ban move, copy allocator and move, copy constructor
+    Resource(const Resource&) = delete;
+    Resource(Resource&&) = delete;
+    Resource& operator=(const Resource&) = delete;
+    Resource& operator=(Resource&&) = delete;
     //! Push the dealloc
     void PushDeletionCall(DeletionType&& deleteCall);
     //! Flush the deletion call in the stakc
