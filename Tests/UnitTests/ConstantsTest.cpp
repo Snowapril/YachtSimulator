1cdd8d52f4e8f29dad091a45bf773056967f70d2
snowapril <sinjihng@naver.com>
2021-06-30 19:43:02 +0900

    feat(dependency) : add unit testing with doctest library

---
+++ Tests/UnitTests/ConstantsTest.cpp
@@ -0,0 +1,9 @@
+#include <doctest/doctest.h>
+#include <Common/Constants.hpp>
+using namespace Common;
+
+TEST_CASE("VkTimeUnit Constants Test")
+{
+    bool result = true;
+    CHECK(result == true);
+}
