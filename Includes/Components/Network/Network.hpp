027c4072ffb837e332cd2fec172198467035e11a
snowapril <sinjihng@naver.com>
2021-07-02 16:15:41 +0900

    feat : add skeleton codes for generate library

---
+++ Includes/Components/Network/Network.hpp
@@ -0,0 +1,19 @@
+#ifndef NETWORK_HPP
+#define NETWORK_HPP
+
+namespace Network
+{
+class Network
+{
+ public:
+    //! Default constructor
+    Network();
+    //! Default destructor
+    ~Network();
+
+ protected:
+ private:
+};
+};  // namespace Network
+
+#endif  //! NETWORK_HPP
---
+++ Sources/Components/Network/Network.cpp
@@ -0,0 +1,14 @@
+#include <Components/Network/Network.hpp>
+
+namespace Network
+{
+Network::Network()
+{
+    //! Do nothing
+}
+Network::~Network()
+{
+    //! Do nothing
+}
+
+};  // namespace Network
