b9a68a3c7f5c0cd5148719c7151b8a6525949d4e
snowapril <sinjihng@naver.com>
2021-07-14 22:21:05 +0900

    feat : implement frame begin & end routine

--- Includes/Components/Renderer/Renderer.hpp
+++ Includes/Components/Renderer/Renderer.hpp
@@ -1,9 +1,9 @@
 #ifndef RENDERER_HPP
 #define RENDERER_HPP
 
+#include <vulkan/vulkan.h>
 #include <Components/Renderer/Resource.hpp>
 #include <Components/Renderer/SwapChain.hpp>
-#include <vulkan/vulkan.h>
 
 namespace Renderer
 {
@@ -13,28 +13,39 @@ class Window;
 class Renderer : public Resource
 {
  public:
-     //! Constructor with pre-generated Device and Window
-    Renderer(std::shared_ptr<Device> devicePtr,
-             std::shared_ptr<Window> windowPtr);
+    //! Constructor with pre-generated Device and Window
+    Renderer(std::shared_ptr<Window> windowPtr,
+             std::shared_ptr<Device> devicePtr);
+
     //! Default destructor
     ~Renderer();
+
     //! Initialize the renderer with the given device and window
-    bool Initialize(std::shared_ptr<Device> devicePtr,
-                    std::shared_ptr<Window> windowPtr);
+    bool Initialize(std::shared_ptr<Window> windowPtr,
+                    std::shared_ptr<Device> devicePtr);
+
     //! Destroy and Recreate swapchain from the deviec
     void RecreateSwapChain(VkExtent2D extent);
+
     //! Begin the rendering frame
     VkCommandBuffer BeginFrame();
+
     //! Begin the swapchain renderpass
     void BeginSwapChainRenderPass(VkCommandBuffer commandBuffer);
+
     //! Finalize the rendering frame
     void EndFrame();
+
     //! Finalize the swapchain renderpass
     void EndSwapChainRenderPass(VkCommandBuffer commandBuffer);
+
  protected:
     std::shared_ptr<Device> _device;
     std::shared_ptr<Window> _window;
     std::unique_ptr<SwapChain> _swapChain;
+    VkClearColorValue _clearColor{ 0.0f, 0.0f, 0.0f, 1.0f };
+    unsigned int _currentImageIndex{ 0 };
+
  private:
 };
 };  // namespace Renderer
--- Sources/Components/Renderer/Renderer.cpp
+++ Sources/Components/Renderer/Renderer.cpp
@@ -1,25 +1,25 @@
-#include <Components/Common/Macros.hpp>
 #include <Components/Renderer/Device.hpp>
 #include <Components/Renderer/Renderer.hpp>
+#include <Components/Renderer/Utils.hpp>
 #include <Components/Renderer/Window.hpp>
 #include <cassert>
 
 namespace Renderer
 {
-Renderer::Renderer(std::shared_ptr<Device> devicePtr,
-                   std::shared_ptr<Window> windowPtr)
+Renderer::Renderer(std::shared_ptr<Window> windowPtr,
+                   std::shared_ptr<Device> devicePtr)
 {
-    assert(Initialize(devicePtr, windowPtr));
+    assert(Initialize(windowPtr, devicePtr));
 }
 Renderer::~Renderer()
 {
+    FlushDeletion();
 }
-bool Renderer::Initialize(std::shared_ptr<Device> devicePtr,
-                          std::shared_ptr<Window> windowPtr)
+bool Renderer::Initialize(std::shared_ptr<Window> windowPtr,
+                          std::shared_ptr<Device> devicePtr)
 {
-    //! Store devicePtr and windowPtr to member variables
-    _device = devicePtr;
     _window = windowPtr;
+    _device = devicePtr;
 
     //! Create the initial swapchain
     RecreateSwapChain(_window->GetScreenSize());
@@ -36,7 +36,7 @@ void Renderer::RecreateSwapChain(VkExtent2D extent)
         {
             LOG_ERROR << "New swapchain image format is not matched with "
                          "original one";
-            abort();
+            std::abort();
         }
     }
     else
@@ -46,21 +46,78 @@ void Renderer::RecreateSwapChain(VkExtent2D extent)
 }
 VkCommandBuffer Renderer::BeginFrame()
 {
+    auto result = _swapChain->AcquireNextImage(&_currentImageIndex);
+    if (result == VK_ERROR_OUT_OF_DATE_KHR)
+    {
+        RecreateSwapChain(_window->GetScreenSize());
+        return nullptr;
+    }
+    VK_CHECK_ERROR(result, "Failed to acquire swap chain image");
+    //! Get current frame command buffer from the device
+    auto cmd = _device->GetCommandBuffer();
     //! Now that we are sure that the commands finished executing,
     //! we can safely reset the commandbuffer to begin recording again
-    // vkResetCommandBuffer()
-    //! TODO(snowapril) : complete this method
-    return nullptr;
+    VK_CHECK_ERROR(vkResetCommandBuffer(cmd, 0),
+                   "Failed to reset command buffer");
+
+    //! Begin the command buffer recording. We will use this command buffer
+    //! exactly once, so we want to let vulkan know that
+    VkCommandBufferBeginInfo cmdBeginInfo = {};
+    cmdBeginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    cmdBeginInfo.pNext = nullptr;
+    cmdBeginInfo.pInheritanceInfo = nullptr;
+    cmdBeginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+    VK_CHECK_ERROR(vkResetCommandBuffer(cmd, 0),
+                   "Failed to reset command buffer");
+
+    VK_CHECK_ERROR(vkBeginCommandBuffer(cmd, &cmdBeginInfo),
+                   "Failed to reset command buffer")
+    return cmd;
 }
 void Renderer::EndFrame()
 {
+    //! Get current frame command buffer from the device
+    auto cmd = _device->GetCommandBuffer();
+    VK_CHECK_ERROR(vkEndCommandBuffer(cmd),
+                   "Failed to complete command buffer recording");
+
+    VK_CHECK_ERROR(_swapChain->SubmitCommandBuffer(cmd, _currentImageIndex),
+                   "Failed to submit command buffer");
 }
 void Renderer::BeginSwapChainRenderPass(VkCommandBuffer commandBuffer)
 {
-    UNUSED_VARIABLE(commandBuffer);
+    static int testFrame = 0;
+    _clearColor.float32[2] = std::abs(std::sin(testFrame++ / 120.0f));
+
+    VkClearValue clearValue = { _clearColor };
+
+    //! Start the main renderpass
+    //! We will use the clear color from above and the framebuffer of the index
+    //! the swapchain gave us
+    VkRenderPassBeginInfo renderPassBeginInfo = {};
+    renderPassBeginInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    renderPassBeginInfo.pNext = nullptr;
+    renderPassBeginInfo.clearValueCount = 1;
+    renderPassBeginInfo.pClearValues = &clearValue;
+    renderPassBeginInfo.renderPass = _swapChain->GetRenderPass();
+    renderPassBeginInfo.renderArea.offset = { 0, 0 };
+    renderPassBeginInfo.renderArea.extent = _window->GetScreenSize();
+
+    if (VkFramebuffer currentFramebuffer =
+            _swapChain->GetFramebuffer(_currentImageIndex))
+        renderPassBeginInfo.framebuffer = currentFramebuffer;
+    else
+    {
+        LOG_ERROR << "Failed to get correct framebuffer from the swapchain";
+        std::abort();
+    }
+
+    vkCmdBeginRenderPass(commandBuffer, &renderPassBeginInfo,
+                         VK_SUBPASS_CONTENTS_INLINE);
 }
 void Renderer::EndSwapChainRenderPass(VkCommandBuffer commandBuffer)
 {
-    UNUSED_VARIABLE(commandBuffer);
+    vkCmdEndRenderPass(commandBuffer);
 }
 };  // namespace Renderer
