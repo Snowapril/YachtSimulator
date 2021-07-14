91b5d2d86d7ee8f4cc00c1ff726d9d858dec71a7
snowapril <sinjihng@naver.com>
2021-07-14 22:21:57 +0900

    feat : add application loop code in main function

--- Sources/Yacht/main.cpp
+++ Sources/Yacht/main.cpp
@@ -1,18 +1,21 @@
-#include <iostream>
 #include <Components/Common/Logger.hpp>
+#include <Yacht/Application.hpp>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
     (void)argc;
     (void)argv;
 
-    std::ofstream file("log.log");
+    std::ofstream file("YachtSimulator.log");
     Common::Logger::Init(Common::Logger::Level::Verbose, &file);
 
-    //! Logging test
-    {
-        LOG_VERBOSE << "Hello Yacht Simulator";
-    }
+    Application application;
+    if (!application.Initialize(1200, 900, "Test"))
+        return 1;
+
+    //! Enter the main loop of the application
+    application.Run();
 
     file.close();
     return 0;
