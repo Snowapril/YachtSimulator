04d996e131fe0bc31b5da65989f17f7d9560bdec
snowapril <sinjihng@gmail.com>
2021-08-05 10:57:42 +0900

    feat : add pipeline class and add shader loading method

--- Includes/Components/Renderer/Pipeline.hpp
+++ Includes/Components/Renderer/Pipeline.hpp
@@ -1,15 +1,35 @@
 #ifndef VK_PIPELINE_HPP
 #define VK_PIPELINE_HPP
 
-#include <Componnents/Renderer/Resource.hpp>
+#include <Components/Renderer/Resource.hpp>
+#include <memory>
 
 namespace Renderer
 {
+class Device;
+
 class Pipeline : public Resource
 {
  public:
+    //! Default constructor
+    Pipeline(std::shared_ptr<Device> devicePtr);
+
+    //! Default destructor
+    ~Pipeline();
+
  protected:
  private:
+    /**
+     * @brief Load spir-V shader binary from given file
+     * 
+     * @param path spir-V shader binary file path
+     * @param outShaderModule pass created shader by reference
+     * @return true if shader loading success return true
+     * @return false if shader loading failed by some reason return false
+     */
+    bool LoadShaderModule(const std::string& path, VkShaderModule* outShaderModule);
+
+    std::shared_ptr<Device> _device;
 };
 };  // namespace Renderer
 
---
+++ Sources/Components/Renderer/Pipeline.cpp
@@ -0,0 +1,63 @@
+#include <vulkan/vulkan.h>
+#include <Components/Renderer/Device.hpp>
+#include <Components/Renderer/Pipeline.hpp>
+#include <fstream>
+
+namespace Renderer
+{
+Pipeline::Pipeline(std::shared_ptr<Device> devicePtr) : _device(devicePtr)
+{
+    //! Do nothing
+}
+
+Pipeline::~Pipeline()
+{
+    FlushDeletion();
+}
+
+bool Pipeline::LoadShaderModule(const std::string& path,
+                                VkShaderModule* outShaderModule)
+{
+    //! Open the file with the cursor at the end
+    std::ifstream file(path, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open())
+        return false;
+
+    //! Find what the size of the file
+    const size_t fileSize = static_cast<size_t>(file.tellg());
+
+    //! Spir-V expects the buffer to be on uint32
+    std::vector<unsigned int> buffer(fileSize / sizeof(unsigned int));
+
+    //! Put file cursor at the begining
+    file.seekg(0);
+
+    //! Read the buffer contents
+    file.read(reinterpret_cast<char*>(buffer.data()), fileSize);
+
+    //! Now buffer reading done
+    file.close();
+
+    //! Create a new shader module using the buffer we loaded
+    VkShaderModuleCreateInfo shaderModuleInfo = {};
+    shaderModuleInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    shaderModuleInfo.pNext = nullptr;
+
+    //! Codesize has to be in bytes
+    shaderModuleInfo.codeSize = buffer.size() * sizeof(unsigned int);
+    shaderModuleInfo.pCode = buffer.data();
+
+    //! Check that the creation goes well
+    VkShaderModule shaderModule;
+
+    if (vkCreateShaderModule(_device->GetDevice(), &shaderModuleInfo, nullptr,
+                             &shaderModule) != VK_SUCCESS)
+        return false;
+
+    //! Now shader loading success
+    *outShaderModule = shaderModule;
+
+    return true;
+}
+};  // namespace Renderer
